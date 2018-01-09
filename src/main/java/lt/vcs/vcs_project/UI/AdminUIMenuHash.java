f66ea5f21d48752c9b951733e2cb676eebc52b17
Giedrius <giedrzav@gmail.com>
2018-01-09 16:47:31 +0200

    updated admin menu options; added student service methods to backend

--- src/main/java/lt/vcs/vcs_project/UI/AdminUIMenuHash.java
+++ src/main/java/lt/vcs/vcs_project/UI/AdminUIMenuHash.java
@@ -8,8 +8,8 @@ public class AdminUIMenuHash {
     public static final Hashtable<String,String> menuOptions = new Hashtable<>() {{
         put("TOP", "\t1 Account Menu\n\t2 Student Menu\n\t3 Lecturer menu\n\t4 Course menu\n\t9 Logout\nPossible options[1-4,9]:");
         put("ACCOUNT", "\t0 Top menu\n\t1 List Accounts\n\t2 Print Account\n\t3 Add Account\n\t4 Update Account\n\t5 Remove account\nPossible options[0-5]:");
-        put("STUDENT", "\t0 Top menu\n\t1 List Students\n\t2 Print Student\n\t3 Add Student\n\t5 Update Student\n\t6 Remove Student\n\t7 Assign course\nPossible options[0-7]:");
-        put("LECTURER", "\t0 Top menu\n\t1 List Lecturers\n\t2 Print Lecturer\n3 Add Lecturer\n\t5 Update Lecturer\n\t6 Remove Lecturer\n\t7 Assign Lecturer\nPossible options[0-7]:");
+        put("STUDENT", "\t0 Top menu\n\t1 List Students\n\t2 Print Student\n\t3 Add Student\n\t4 Update Student\n\t5 Remove Student\n\t6 Assign course\nPossible options[0-6]:");
+        put("LECTURER", "\t0 Top menu\n\t1 List Lecturers\n\t2 Print Lecturer\n3 Add Lecturer\n\t4 Update Lecturer\n\t5 Remove Lecturer\n\t6 Assign Lecturer\nPossible options[0-6]:");
         put("COURSE", "\t0 Top menu\n\t1 List Courses\n\t2 Print Course\n\t3 Add Course\n\t4 Update Course\n\t5 Remove Course\n\t6 Assign Course\nPossible options[0-6]:");
     }};
 
@@ -25,7 +25,25 @@ public class AdminUIMenuHash {
         put("ACCOUNT", "4","UPDATE_ACCOUNT");
         put("ACCOUNT", "5","REMOVE_ACCOUNT");
         put("STUDENT", "0","TOP");
+        put("STUDENT", "1", "LIST_STUDENTS");
+        put("STUDENT", "2", "PRINT_STUDENT");
+        put("STUDENT", "3", "ADD_STUDENT");
+        put("STUDENT", "4", "UPDATE_STUDENT");
+        put("STUDENT", "5", "REMOVE_STUDENT");
+        put("STUDENT", "6", "ASSIGN_COURSE");
         put("LECTURER", "0","TOP");
+        put("LECTURER", "1", "LIST_LECTURERS");
+        put("LECTURER", "2", "PRINT_LECTURER");
+        put("LECTURER", "3", "ADD_LECTURER");
+        put("LECTURER", "4", "UPDATE_LECTURER");
+        put("LECTURER", "5", "REMOVE_LECTURER");
+        put("LECTURER", "6", "ASSIGN_COURSE");
         put("COURSE", "0","TOP");
+        put("COURSE", "1", "LIST_COURSES");
+        put("COURSE", "2", "PRINT_COURSE");
+        put("COURSE", "3", "ADD_COURSE");
+        put("COURSE", "4", "UPDATE_COURSE");
+        put("COURSE", "5", "REMOVE_COURSE");
+        put("COURSE", "6", "ASSIGN_COURSE");
     }};
 }
--- src/main/java/lt/vcs/vcs_project/backend/Backend.java
+++ src/main/java/lt/vcs/vcs_project/backend/Backend.java
@@ -4,10 +4,8 @@ import lt.vcs.vcs_project.utils.ScannerUtils;
 
 public class Backend {
     public static AccountServices accounts = new AccountServices();
-   /* public void initialize(){
-       // accounts = new AccountServices();
-        new StudentServices();
-    }*/
+    public static StudentServices students = new StudentServices();
+
 
     public static String login() {
         System.out.printf("\n\nWelcome! Please login.\n");
@@ -17,7 +15,7 @@ public class Backend {
             System.out.print("Password: ");
             String password = ScannerUtils.scanString();
 
-            if (accounts.authenticate(username,password) ) {
+            if (accounts.authenticate(username, password)) {
                 return username;
             }
             System.out.println("Login failed, please try again");
@@ -26,7 +24,7 @@ public class Backend {
 
     public static String loginDefault() {
         System.out.printf("\n\nWelcome! Admin.\n");
-          return "admin";
+        return "admin";
     }
 
     public static Role getRole(String loginId) {
@@ -41,23 +39,36 @@ public class Backend {
         return accounts.getAccount(loginId).getSecondName();
     }
 
-    public static void listAccounts(){
+    public static void listAccounts() {
         System.out.println(accounts.listAccounts());
     }
 
-    public static void printAccount(String loginId){
+    public static void printAccount(String loginId) {
         System.out.println(accounts.getAccount(loginId).toString());
     }
 
-    public static boolean accountExists(String  loginId){
+    public static boolean accountExists(String loginId) {
         return accounts.containsKey(loginId);
     }
 
-    public static boolean accountAdminExists(String  loginId){
-        return (accounts.containsKey(loginId) && accounts.getAccount(loginId).getRole()==Role.ADMIN);
+    public static boolean accountAdminExists(String loginId) {
+        return (accounts.containsKey(loginId) && accounts.getAccount(loginId).getRole() == Role.ADMIN);
     }
 
-    public static void addAccount(String csv){
+    public static void addAccount(String csv) {
         accounts.addAccount(csv);
+    }
+
+
+    public static void listStudents() {
+        System.out.println(students.listAccounts());
+    }
+
+    public static void printStudent(String loginId) {
+        System.out.println(students.getAccount(loginId).toString());
+    }
+
+    public static boolean studentExists(String loginId) {
+        return students.containsKey(loginId);
     }
 }
